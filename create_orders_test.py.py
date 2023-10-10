488ff39a4525b07e5b7c59651e550a8ea84e00a1
Natalia3223 <143688382+Natalia3223@users.noreply.github.com>
2023-10-10 07:38:32 +0300

    Update create_orders_test.py.py

--- create_orders_test.py.py
+++ create_orders_test.py.py
@@ -4,8 +4,8 @@ import data
 # Наталья Волкова, 08 а когорта — Финальный проект. Инженер по тестированию плюс
 
 
-def test_get_orders_track():
+def test_get_orders_track(): # Получение данных о заказе по треку
     track = sender_stand_request.post_orders_body(data.orders_body).json()['track']
     response = sender_stand_request.get_orders_track(track)
 
-    assert response.status_code == 200 + int(response.status_code)
+    assert response.status_code == 200  # Проверка кода ответа
